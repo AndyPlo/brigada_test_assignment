404308e6e020384c07e4d0f3acc6aa8cfc69539c
Andrei Plotnikov <avin@mail.ru>
2022-10-30 22:11:36 +0500

    Models done

--- brigada_ta/objects/admin.py
+++ brigada_ta/objects/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brigada, City, Object
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('city_name',)
+
+
+@admin.register(Brigada)
+class BrigadaAdmin(admin.ModelAdmin):
+    list_display = (
+        'brigada_name',
+        'people_amount',
+        'name_responsible',
+        'position_responsible',
+        'rank_responsible',
+        'city',
+    )
+
+
+@admin.register(Object)
+class ObjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'object_name',
+        'people_amount',
+        'name_responsible',
+        'position_responsible',
+        'rank_responsible',
+        'brigada',
+        'city',
+    )
--- brigada_ta/objects/models.py
+++ brigada_ta/objects/models.py
@@ -1,3 +1,118 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    city_name = models.CharField(
+        'Название города',
+        max_length=30,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ['city_name']
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.city_name
+
+
+class Brigada(models.Model):
+    brigada_name = models.CharField(
+        'Бригада',
+        max_length=30,
+        unique=True
+    )
+    people_amount = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    name_responsible = models.CharField(
+        'Ф.И.О. ответственного',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    position_responsible = models.CharField(
+        'Должность ответственного',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    rank_responsible = models.IntegerField(
+        'Разряд ответственного',
+        validators=[MinValueValidator(1), MaxValueValidator(8)],
+        blank=True,
+        null=True
+    )
+    city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='brigadas',
+        verbose_name='Город'
+    )
+
+    class Meta:
+        ordering = ['brigada_name']
+        verbose_name = 'Бригада'
+        verbose_name_plural = 'Бригады'
+
+    def __str__(self):
+        return f'{self.brigada_name} ({self.city})'
+
+
+class Object(models.Model):
+    object_name = models.CharField(
+        'Объект',
+        max_length=30,
+        unique=True
+    )
+    people_amount = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    name_responsible = models.CharField(
+        'Ф.И.О. ответственного',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    position_responsible = models.CharField(
+        'Должность ответственного',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    rank_responsible = models.IntegerField(
+        'Разряд ответственного',
+        validators=[MinValueValidator(1), MaxValueValidator(8)],
+        blank=True,
+        null=True
+    )
+    brigada = models.ForeignKey(
+        Brigada,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='objects',
+        verbose_name='Бригада'
+    )
+    city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='objects',
+        verbose_name='Город'
+    )
+
+    class Meta:
+        ordering = ['object_name']
+        verbose_name = 'Объект'
+        verbose_name_plural = 'Объекты'
+
+    def __str__(self):
+        return f'{self.object_name} ({self.city})'
